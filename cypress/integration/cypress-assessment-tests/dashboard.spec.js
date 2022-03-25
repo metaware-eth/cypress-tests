d651fe21b7691d73c9b565a26934375d86f15143
metaware-eth <metaware.eth@gmail.com>
2022-03-25 14:14:20 -0700

    add dashboard tests

---
+++ cypress/integration/cypress-assessment-tests/dashboard.spec.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+require('cypress-xpath')
+
+
+describe('cypress assessment dashboard tests', () => {
+    beforeEach(() => {
+        // Assume successful login
+        cy.visit('http://localhost:3000/')
+
+        cy.get('#email').type('me@example.com').should('have.value', 'me@example.com')
+        cy.get('#password').type('password').should('have.value', 'password')
+
+        cy.xpath('//*[@id="__next"]/div/div[2]/div/form/div[4]/button').click()
+
+        cy.location('pathname').should('eq', '/dashboard')      
+    })
+  
+    it('table header items exists', () => {
+        let values = []
+        
+        cy.get('thead > tr')
+          .find('th')
+          .each(($el) => {
+             cy.wrap($el)
+              .invoke('text')
+              .then(text => {
+                values.push(text.trim())
+                  })
+               })
+              .then(() => expect(values).to.deep.eq(
+                  ["ID", "Name", "Email", "Created_at", "Edit", "Delete"]))
+    })
+
+    it('table data items exists', () => {
+        let values = []
+        
+        cy.get('tbody > tr')
+          .find('td')
+          .each(($el) => {
+             cy.wrap($el)
+              .invoke('text')
+              .then(text => {
+                values.push(text.trim())
+                  })
+               })
+              .then(() => expect(values).to.deep.eq(
+                  ["1", "Jon doe", "jhondoe@example.com", "2021-1-12", "Edit", "Delete", "1", "Jon doe", "jhondoe@example.com", "2021-1-12", "Edit", "Delete", "1", "Jon doe", "jhondoe@example.com", "2021-1-12", "Edit", "Delete"]))
+    })    
+  })
+  
--- cypress/integration/cypress-assessment-tests/side_menu.spec.js
+++ cypress/integration/cypress-assessment-tests/side_menu.spec.js
@@ -17,42 +17,43 @@ describe('cypress assessment side menu tests', () => {
   
     it('dashboard item exists', () => {
         // Check element and text
-        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[1]/a/span[2]').invoke('text').then((text) => {
+        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[1]/a/span[2]').should('be.visible').invoke('text').then((text) => {
             expect(text.trim()).equal('Dashboard')
         })
     })
 
     it('profile item exists', () => {
         // Check element and text
-        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[2]/a/span[2]').invoke('text').then((text) => {
+        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[2]/a/span[2]').should('be.visible').invoke('text').then((text) => {
             expect(text.trim()).equal('Profile')
         })
     })
 
     it('notifications item exists', () => {
         // Check element and text
-        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[3]/a/span[2]').invoke('text').then((text) => {
+        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[3]/a/span[2]').should('be.visible').invoke('text').then((text) => {
             expect(text.trim()).equal('Notifications')
         })
     })
 
     it('logout item exists', () => {
         // Check element and text
-        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[4]/a/span[2]').invoke('text').then((text) => {
+        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[4]/a/span[2]').should('be.visible').invoke('text').then((text) => {
             expect(text.trim()).equal('Logout')
         })
     })
 
     it('notification alert item exists', () => {
         // Check element and text
-        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[3]/a/span[3]').invoke('text').then((text) => {
+        cy.xpath('//*[@id="__next"]/div/div[1]/ul/li[3]/a/span[3]').should('be.visible').invoke('text').then((text) => {
             expect(text.trim()).equal('5')
         })
     })    
 
     it('logo item exists', () => {
-        // Check element and text
+        // Check element, src, and alt
         cy.xpath('//*[@id="__next"]/div/div[1]/div/img').should('have.attr', 'src').should('include','tailwindui.com/img/logos/workflow-mark-indigo-600.svg')
+        cy.xpath('//*[@id="__next"]/div/div[1]/div/img').should('have.attr', 'alt').should('include','Workflow')
     })    
   })
   
